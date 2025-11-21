51635b830c1a9bca15b26f082af894b911bf332d
Piushan wickramasinghe <99070586+piushan003@users.noreply.github.com>
2025-11-21 08:36:10 +0530

    Update_1

---
+++ android/app/src/main/kotlin/com/example/amrich/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.amrich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+     MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal.shade200,
+        appBar: AppBar(backgroundColor: Colors.teal,
+        title:Center(
+          child: Text('I Am Rich', style: TextStyle(color: Colors.white)),
+          ) 
+      ),
+        body: Center(
+          child: Image.asset('images/ruby.png'), 
+          ),
+    ),
+   ),
+  );
+}
